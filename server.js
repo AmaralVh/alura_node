69c602861653ae16185b460e9c3f8bf6571c30b4
Victor Hugo Amaral <amaral.vht@gmail.com>
2023-04-28 09:49:15 -0300

    from http to express

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pagina de editora',
-    '/sobre': 'info sobre o projeto'
-}
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
 
-server.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor escutando em http://localhost:${PORT}`);
 });
