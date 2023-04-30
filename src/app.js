23c4bc1552948cf70143e8107fbbde35b29f5a34
Victor Hugo Amaral <amaral.vht@gmail.com>
2023-04-30 10:36:36 -0300

    novas requisições

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express(); // instanciando express
 
+// Setando interpretação por json:
+app.use(express.json())
+
+
 const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
     {id: 2, "titulo": "O Hobbit"}
@@ -15,6 +19,33 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id); // funcao que retorna o livro com id passado pela req.
+    res.json(livros[index]);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso');
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id); // funcao que retorna o livro com id passado pela req.
+    livros[index].titulo = req.body.titulo; // atualiza o titulo do livro em questao.
+    res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id); // funcao que retorna o livro com id passado pela req.
+    livros.splice(index, 1) // apagando o livro (1 sendo quantos elemento eu quero excluir a partir do index).
+    res.send(`Livro ${id} foi removido com sucesso`);
+})
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id);
+}
 
 
 export default app;
